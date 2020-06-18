ef1fc4c283528a6953674eb063fa2ec731845772
Leandro Cardozo Alves <lca000@gmail.com>
2020-06-18 00:27:03 -0300

    crawler_cei: Remove posfixo Fracionado(F) do ticker no df

--- src/crawler_cei.py
+++ src/crawler_cei.py
@@ -112,8 +112,12 @@ class CrawlerCei():
             else:
                 return 'Compra'
 
+        def remove_fracionado_ticker(ticker):
+            return ticker[:-1] if ticker.endswith('F') else ticker
+
         df['data'] = pd.to_datetime(df['data'], dayfirst=True)
         df['data'] = df['data'].dt.date
+        df['ticker'] = df.apply(lambda row: remove_fracionado_ticker(row.ticker), axis=1)
         df['operacao'] = df.apply(lambda row: formata_compra_venda(row.operacao), axis=1)
         df['qtd_ajustada'] = df.apply(lambda row: calculate_add(row), axis=1)
 
