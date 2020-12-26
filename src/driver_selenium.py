6e33148ed6fb4c8d4981137e42c166c0336eeffc
Guilherme Silveira <guilherme.silveira@thoughtworks.com>
2020-12-26 16:45:50 -0300

    options

--- src/driver_selenium.py
+++ src/driver_selenium.py
@@ -11,6 +11,7 @@ class ChromeDriver(object):
     def __configure_driver(headless):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
         if headless:
             chrome_options.add_argument('--headless')
         chrome_options.add_argument('--disable-gpu')
